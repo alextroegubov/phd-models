989e0361d0a4ceed052efdaf3738a94bc6b179fc
troegubov <troegubov@wireless.iitp.ru>
2025-01-13 13:38:57 +0300

    feat(utils.py): add dataclass for metrics

--- paper-satelite-simple/utils.py
+++ paper-satelite-simple/utils.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Optional
 
 @dataclass
 class ParametersSet:
@@ -13,3 +14,20 @@ class ParametersSet:
     data_mu: float
 
     beam_capacity: int
+
+@dataclass
+class Metrics:
+    # pi_k, k=1,...,n and pi_e
+    rt_request_rej_prob: list[float] = None
+    data_request_rej_prob: float = 0
+    # y_k, k=1,...n and y_e
+    mean_rt_requests_in_service: list[float] = None
+    mean_data_requests_in_service: float = 0
+    # m_k, k=1,...n and m_e
+    mean_resources_per_rt_flow: list[float] = None
+    mean_resources_per_data_flow: float = 0
+    # W and b_e
+    mean_data_request_service_time: float = 0
+    mean_resources_per_data_request: float = 0
+
+    beam_utilization: float = 0
