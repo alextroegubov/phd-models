6f56cb1b07a1cbbf3910d2bcf97c822b630d1ccd
troegubov <troegubov@wireless.iitp.ru>
2025-01-13 21:19:38 +0300

    feat(utils.py): add __str__ method for metrics + file formatting

--- paper-satelite-simple/utils.py
+++ paper-satelite-simple/utils.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional
 
+
 @dataclass
 class ParametersSet:
     real_time_flows: int
@@ -14,6 +15,7 @@ class ParametersSet:
     data_mu: float
 
     beam_capacity: int
+
 
 @dataclass
 class Metrics:
@@ -31,3 +33,29 @@ class Metrics:
     mean_resources_per_data_request: float = 0
 
     beam_utilization: float = 0
+
+    text: str = ""
+
+    def __str__(self):
+
+        def f_lst_5f(lst):
+            return ", ".join(f"{x:.5f}" for x in lst) if lst else "[]"
+
+        def f_lst_3f(lst):
+            return ", ".join(f"{x:.3f}" for x in lst) if lst else "[]"
+
+        return (
+            f"Metrics: {self.text}\n"
+            f"  Overall:\n"
+            f"      Beam Utilization            : {self.beam_utilization:.4f}\n"
+            f"  Real-time flows:\n"
+            f"      Request rejection prob.     : [{f_lst_5f(self.rt_request_rej_prob)}]\n"
+            f"      Mean requests in service    : [{f_lst_3f(self.mean_rt_requests_in_service)}]\n"
+            f"      Mean resources per flow     : [{f_lst_3f(self.mean_resources_per_rt_flow)}]\n"
+            f"  Elastic data flow:\n"
+            f"      Request rejection prob.     : {self.data_request_rej_prob:.4f}\n"
+            f"      Mean requests in Service    : {self.mean_data_requests_in_service:.4f}\n"
+            f"      Mean resources per flow     : {self.mean_resources_per_data_flow:.4f}\n"
+            f"      Mean resources per request  : {self.mean_resources_per_data_request:.4f}\n"
+            f"      Mean request service time   : {self.mean_data_request_service_time:.4f}\n"
+        )
