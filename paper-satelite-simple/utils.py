cb7d6ad3f1408d0ab5da5efc0753c487eded5027
troegubov <troegubov@wireless.iitp.ru>
2025-01-18 12:25:19 +0300

    refactor(utils.py): remove Optional

--- paper-satelite-simple/utils.py
+++ paper-satelite-simple/utils.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import Optional
 
 
 @dataclass
@@ -38,17 +37,21 @@ class ParametersSet:
 
 @dataclass
 class Metrics:
-    # pi_k, k=1,...,n and pi_e
-    rt_request_rej_prob: Optional[list[float]] = None
+    # pi_k, k=1,...,n
+    rt_request_rej_prob: list[float]
+    # y_k, k=1,...n
+    mean_rt_requests_in_service: list[float]
+    # m_k, k=1,...n
+    mean_resources_per_rt_flow: list[float]
+    # pi_e
     data_request_rej_prob: float = 0
-    # y_k, k=1,...n and y_e
-    mean_rt_requests_in_service: Optional[list[float]] = None
+    # y_e
     mean_data_requests_in_service: float = 0
-    # m_k, k=1,...n and m_e
-    mean_resources_per_rt_flow: Optional[list[float]] = None
+    # m_e
     mean_resources_per_data_flow: float = 0
-    # W and b_e
+    # W
     mean_data_request_service_time: float = 0
+    # b_e
     mean_resources_per_data_request: float = 0
 
     beam_utilization: float = 0
